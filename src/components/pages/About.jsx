19e1c3e247ce234441f728a98506c675b015d5b4
davisnate04 <davisnate04@gmail.com>
2024-04-23 17:00:16 -0400

    Updated About me

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -6,10 +6,7 @@ export default function About() {
             </h3>
             <img className="avatar" src="https://avatars.githubusercontent.com/u/111401054?v=4"></img>
             <p className="aboutMe">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <p className="aboutMe">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Aliquam faucibus purus in massa. Egestas integer eget aliquet nibh praesent tristique magna. Tempus urna et pharetra pharetra massa massa ultricies mi. Nec tincidunt praesent semper feugiat nibh sed pulvinar. Lacus sed viverra tellus in hac habitasse platea dictumst. Sed odio morbi quis commodo odio aenean sed adipiscing. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula. Purus sit amet luctus venenatis lectus magna fringilla.
+            I am a web developer that has experience with developing in the frontend, but mostly with the backend. The proficiences I have are listed in the resume page of my website. I am currently taking a course in full stack development with the hopes of making a profession out of what I have learned.
             </p>
         </div>
     )
